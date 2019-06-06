7798f217b485391ce04874cba3336d42ff380461
boruch d <dubywider@gmail.com>
2019-06-06 17:14:35 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,7 +13,9 @@ end
 def display_balance
  "Your balance is $#{self.balance}."
 end 
-
+def valid?
+    self.balance > 0 && self.status == "open" ? true : false
+  end
  def close_account
     self.status = "closed"
   end
