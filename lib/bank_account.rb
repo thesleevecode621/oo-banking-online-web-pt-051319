9a4c836fe9f2b6b418f19a8e992ce575754b4cb9
boruch d <dubywider@gmail.com>
2019-06-06 16:51:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
 attr_reader :name 
 attr_accessor :balance,:status 
-def initialize(name,balance=1000 status = "open")
+def initialize(name,balance=1000 status = open)
   @name = name 
   @balance = balance
   @status = status
