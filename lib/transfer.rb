ba3b0b19028393b701d2e304a770dd0ef380c1cb
boruch d <dubywider@gmail.com>
2019-06-06 17:30:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,8 +10,31 @@ class Transfer
   def valid?
     sender.valid? && receiver.valid?
   end
+
 def execute_transaction
-if valid? && balance > amount && self.status == pending
-  
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+   def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.deposit(self.amount)
+      receiver.withdrawal(self.amount)
+      self.status = "reversed"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."      
+    end
+  end
+
+
+
+ end	end
 end
 end 
